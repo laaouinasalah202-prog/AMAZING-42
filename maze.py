0761b49147a6057ad5874c790a77824e86d25cb5
Salah Eddine Laaouina <slaaouin@c1r9p3.1337.ma>
2026-01-17 11:43:06 +0100

    initial maze

---
+++ maze.py
@@ -0,0 +1,40 @@
+def intitial_map(width: int, high: int):
+    cell = {
+        "visited": False,
+        "north": 1,
+        "west": 1,
+        "east": 1,
+        "south": 1
+    }
+
+    maze = []
+    for r in range(high):
+        row = []
+        for col in range(width):
+            row.append(cell)
+        maze.append(row)
+    return maze
+
+maze = intitial_map(20, 20)
+m = maze[0]
+high = 20
+width = 20
+print("+" + "---+" * (width + 2))
+for h in range(high):
+    top_line = "+"
+    bottom_line = "+"
+    middle_line = ""
+    for w in range(width):
+        if maze[h][w]["north"]:
+            top_line += "---+"
+
+        if maze[h][w]["west"]:
+            middle_line += "|   "
+
+        if maze[h][w]["south"]:
+            bottom_line += "---+"
+    middle_line += "|   #"
+    print("    " + top_line)
+    print("#   " + middle_line)
+print("    " + bottom_line )
+print("+" + "---+" * (width + 2))
