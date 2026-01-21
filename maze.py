01892343be96356811d15b44b6f39fdf0049db08
Salah Eddine Laaouina <slaaouin@c2r4p1.1337.ma>
2026-01-21 11:14:56 +0100

    change display

--- maze.py
+++ maze.py
@@ -1,11 +1,31 @@
 import random
+import os
 
+def print_maze(maze):
+    line = "+"
+    for x in range(15):
+        line += "---+"
+    print(line)
+
+    for y in range(15):
+        line1 = "|"
+        line2 = "+"
+        for x in range(15):
+            cell = maze[x][y]["Walls"]
+            if sum(cell) == 4: 
+                line1 += "\033[41m   \033[0m"
+            else:
+                line1 += "   "
+            line1 += "|" if cell[1] else " "
+            line2 += "---+" if cell[2] else "   +"
+        print(line1)
+        print(line2)
+
+
+def show_maze(maze):
+    
 
 def intitial_map(width: int, high: int):
-    cell = {
-
-    }
-
     maze = []
     for r in range(high):
         row = []
@@ -20,7 +40,7 @@ def intitial_map(width: int, high: int):
 
 
 def maze_build(maze):
-    
+
     visited = [[False for i in maze[0]] for a in maze]
 
     dirs = [
@@ -29,54 +49,36 @@ def maze_build(maze):
     (0, 1, 2, 0),   # down
     (-1, 0, 3, 1)   # left
     ]
+
     def check_boundry(x, y):
         return 0 <= x < len(maze) and 0 <= y < len(maze[0])
 
     def backtrack(x, y):
-        visited[x][y] = True
-        maze[x][y]["visited"] = True
+        print_maze(maze)
+        # time.sleep(0.05)
+        os.system("clear")
+        # random.shuffle(dirs)
 
-        random.shuffle(dirs)        
-        
-        for dx, dy, wall, opp_wall  in dirs:
+        visited[x][y] = True
+
+        for dx, dy, wall, opp_wall  in random.sample(dirs, len(dirs)):
             nx, ny = x + dx, y + dy
             if check_boundry(nx, ny) and not visited[nx][ny]:
-                maze[x][y]["Walls"][wall] = False
-                maze[nx][ny]["Walls"][opp_wall] = False
+                maze[x][y]["Walls"][wall] = 0
+                maze[nx][ny]["Walls"][opp_wall] = 0
                 backtrack(nx, ny)
     backtrack(0, 0)
     return maze
 
-def display_maze(maze):
 
-    m = maze[0]
-    high = len(maze)
-    width = len(maze[0])
-    print("+" + "---+" * (width + 2))
-    for h in range(high):
-        top_line = "+"
-        bottom_line = "+"
-        middle_line = ""
-        for w in range(width):
-            if maze[h][w]["Walls"][0]:
-                top_line += "---+"
-            else:
-                top_line += "   +"
-            if maze[h][w]["Walls"][1]:
-                middle_line += "|   "
-            else:
-                middle_line += "    "
-            if maze[h][w]["Walls"][2]:
-                bottom_line += "---+"
-            else:
-                bottom_line += "   +"
-        middle_line += "|   #"
-        print("    " + top_line)
-        print("#   " + middle_line)
-    print("    " + bottom_line )
-    print("+" + "---+" * (width + 2))
+import time
+maze = intitial_map(15, 15)
+maze = maze_build(maze) 
+print_maze(maze)
 
-
-maze = intitial_map(10, 10)
-# maze = maze_build(maze) 
-display_maze(maze)
+for row in maze:
+    for col in row:
+        col = [str(c) for c in col["Walls"]]
+        a = ''.join(col)
+        print(f"{hex(int(a, 2))[2:]}  ", end='')
+    print()
